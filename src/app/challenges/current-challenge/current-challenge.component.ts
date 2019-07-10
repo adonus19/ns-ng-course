e0b5960e655695028f6a94ffd438ed979483035c
dpogue <daniel.pogue@aig.com>
2019-07-10 11:27:49 -0400

    modal setup done; lazy loading next

--- src/app/challenges/current-challenge/current-challenge.component.ts
+++ src/app/challenges/current-challenge/current-challenge.component.ts
@@ -1,5 +1,8 @@
-import { Component } from "@angular/core";
+import { Component, ViewContainerRef } from "@angular/core";
 import { RouterExtensions } from "nativescript-angular/router";
+import { ModalDialogService } from "nativescript-angular/modal-dialog";
+import { DayModalComponent } from "../day-modal/day-modal.component";
+import { UIService } from "~/app/shared/ui.service";
 
 @Component({
   selector: 'ns-current-challenge',
@@ -9,10 +12,20 @@ import { RouterExtensions } from "nativescript-angular/router";
 })
 export class CurrentChallengeComponent {
 
-  constructor(private router: RouterExtensions) { }
+  constructor(private router: RouterExtensions, private modalDialog: ModalDialogService,
+    private vcRef: ViewContainerRef, private uiService: UIService) { }
 
   onAddChallenge() {
-    this.router.navigate(['/challenges/edit'], { transition: { name: 'slideLeft' } });
+    // this.router.navigate(['/challenges/edit'], { transition: { name: 'slideLeft' } });
+  }
+
+  onChangeStatus() {
+    this.modalDialog.showModal(DayModalComponent, {
+      fullscreen: true, viewContainerRef: this.uiService.getRootVCRef() ? this.uiService.getRootVCRef() : this.vcRef,
+      context: { date: new Date() }
+    }).then((action: string) => {
+      console.log(action);
+    });
   }
 
 
