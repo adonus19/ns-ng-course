4ccf88bcf771f9ae1cf3fe0d37c58bedee65dce1
dpogue <daniel.pogue@aig.com>
2019-07-10 10:41:24 -0400

    passing dynamic data with paramMap

--- src/app/challenges/challenge-edit/challenge-edit.component.ts
+++ src/app/challenges/challenge-edit/challenge-edit.component.ts
@@ -9,14 +9,17 @@ import { PageRoute } from 'nativescript-angular/router';
   moduleId: module.id,
 })
 export class ChallengeEditComponent implements OnInit {
+  isCreating = true;
+
   constructor(private activatedRoute: ActivatedRoute, private pageRoute: PageRoute) { }
   ngOnInit() {
-    // this.activatedRoute.paramMap.subscribe(paramMap => {
-    //   console.log(paramMap.get('mode'));
-    // });
     this.pageRoute.activatedRoute.subscribe(activatedRoute => {
       activatedRoute.paramMap.subscribe(paramMap => {
-        console.log(paramMap.get('mode'));
+        if (!paramMap.has('mode')) {
+          this.isCreating = true;
+        } else {
+          this.isCreating = paramMap.get('mode') !== 'edit';
+        }
       });
     })
   }
