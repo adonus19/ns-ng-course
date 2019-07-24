2f92a3826afa1efb8ff531c5e15e8ae2876e1178
dpogue <daniel.pogue@aig.com>
2019-07-23 21:47:50 -0400

    #138

--- src/app/challenges/challenge-edit/challenge-edit.module.ts
+++ src/app/challenges/challenge-edit/challenge-edit.module.ts
@@ -1,12 +1,14 @@
 import { NgModule, NO_ERRORS_SCHEMA } from "@angular/core";
-import { ChallengeEditComponent } from "./challenge-edit.component";
 import { NativeScriptCommonModule } from "nativescript-angular/common";
-import { SharedModule } from "~/app/shared/ui/shared.module";
 import { NativeScriptRouterModule } from "nativescript-angular/router";
+import { NativeScriptFormsModule } from "nativescript-angular/forms";
+import { SharedModule } from "~/app/shared/ui/shared.module";
+import { ChallengeEditComponent } from "./challenge-edit.component";
 
 @NgModule({
   declarations: [ChallengeEditComponent],
   imports: [NativeScriptCommonModule, SharedModule,
+    NativeScriptFormsModule,
     //  NativeScriptRouterModule   <--- must be imported if I want to use nsRouterLink, yes double import. Watch video #105
     NativeScriptRouterModule.forChild([{ path: '', component: ChallengeEditComponent }])],
   schemas: [NO_ERRORS_SCHEMA]
